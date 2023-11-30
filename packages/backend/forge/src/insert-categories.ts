020887681bc088268a1fa7d7d6fa7723ddb7911d
Mike Xiong <xiong.tchylon@gmail.com>
2023-11-30 17:25:44 +0100

    add svg upload

--- packages/backend/forge/src/insert-categories.ts
+++ packages/backend/forge/src/insert-categories.ts
@@ -5,55 +5,58 @@ await db
   .values([
     {
       name: 'art',
-      logo_path: 'art.svg',
+      logo_path: '/packages/frontend/web/public/uploads/categories/art.svg',
     },
     {
       name: 'cooking',
-      logo_path: 'cooking.svg',
+      logo_path: '/packages/frontend/web/public/uploads/categories/cooking.svg',
     },
     {
       name: 'dancing',
-      logo_path: 'dancing.svg',
+      logo_path: '/packages/frontend/web/public/uploads/categories/dancing.svg',
     },
     {
       name: 'gaming',
-      logo_path: 'gaming.svg',
+      logo_path: '/packages/frontend/web/public/uploads/categories/gaming.svg',
     },
     {
       name: 'gardening',
-      logo_path: 'gardening.svg',
+      logo_path:
+        '/packages/frontend/web/public/uploads/categories/gardening.svg',
     },
     {
       name: 'hiking',
-      logo_path: 'hiking.svg',
+      logo_path: '/packages/frontend/web/public/uploads/categories/hiking.svg',
     },
     {
       name: 'music',
-      logo_path: 'music.svg',
+      logo_path: '/packages/frontend/web/public/uploads/categories/music.svg',
     },
     {
       name: 'outings',
-      logo_path: 'outings.svg',
+      logo_path: '/packages/frontend/web/public/uploads/categories/outings.svg',
     },
     {
       name: 'photography',
-      logo_path: 'photography.svg',
+      logo_path:
+        '/packages/frontend/web/public/uploads/categories/photography.svg',
     },
     {
       name: 'reading',
-      logo_path: 'reading.svg',
+      logo_path: '/packages/frontend/web/public/uploads/categories/reading.svg',
     },
     {
       name: 'sports',
-      logo_path: 'sports.svg',
+      logo_path: '/packages/frontend/web/public/uploads/categories/sports.svg',
     },
     {
       name: 'traveling',
-      logo_path: 'traveling.svg',
+      logo_path:
+        '/packages/frontend/web/public/uploads/categories/traveling.svg',
     },
     {
       name: 'writing',
-      logo_path: 'writing.svg',
+      logo_path: '/packages/frontend/web/public/uploads/categories/writing.svg',
     },
   ])
   .execute();
