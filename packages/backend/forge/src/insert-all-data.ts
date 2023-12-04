a2506ce0d9854fe54e4b465fa69cf910bcc5adae
Mike Xiong <xiong.tchylon@gmail.com>
2023-12-04 15:57:48 +0100

    remove insert all data

--- packages/backend/forge/src/insert-all-data.ts
+++
@@ -1,9 +0,0 @@
-import { insertCategories } from './insert-categories';
-
-const insertAllData = async () => {
-  await Promise.all([insertCategories()]);
-};
-
-await insertAllData().catch((error) => {
-  throw error;
-});
