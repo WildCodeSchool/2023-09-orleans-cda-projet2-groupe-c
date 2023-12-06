2c295b17ac4147fc50e4dbae1ce7d1da9ff2151e
Mike Xiong <xiong.tchylon@gmail.com>
2023-12-06 15:00:34 +0100

    fix: update path logo

--- packages/backend/forge/src/hobbies/insert-categories.ts
+++ packages/backend/forge/src/hobbies/insert-categories.ts
@@ -6,51 +6,51 @@ export const insertCategories = async () => {
     .values([
       {
         name: 'art',
-        logo_path: '/uploads/categories/art.svg',
+        logo_path: '/categories-logos/categories/art.svg',
       },
       {
         name: 'cooking',
-        logo_path: '/uploads/categories/cooking.svg',
+        logo_path: '/categories-logos/categories/cooking.svg',
       },
       {
         name: 'dancing',
-        logo_path: '/uploads/categories/dancing.svg',
+        logo_path: '/categories-logos/categories/dancing.svg',
       },
       {
         name: 'gaming',
-        logo_path: '/uploads/categories/gaming.svg',
+        logo_path: '/categories-logos/categories/gaming.svg',
       },
       {
         name: 'gardening',
-        logo_path: '/uploads/categories/gardening.svg',
+        logo_path: '/categories-logos/categories/gardening.svg',
       },
       {
         name: 'music',
-        logo_path: '/uploads/categories/music.svg',
+        logo_path: '/categories-logos/categories/music.svg',
       },
       {
         name: 'outing',
-        logo_path: '/uploads/categories/outings.svg',
+        logo_path: '/categories-logos/categories/outings.svg',
       },
       {
         name: 'photography',
-        logo_path: '/uploads/categories/photography.svg',
+        logo_path: '/categories-logos/categories/photography.svg',
       },
       {
         name: 'reading',
-        logo_path: '/uploads/categories/reading.svg',
+        logo_path: '/categories-logos/categories/reading.svg',
       },
       {
         name: 'sport',
-        logo_path: '/uploads/categories/sports.svg',
+        logo_path: '/categories-logos/categories/sports.svg',
       },
       {
         name: 'traveling',
-        logo_path: '/uploads/categories/traveling.svg',
+        logo_path: '/categories-logos/categories/traveling.svg',
       },
       {
         name: 'writing',
-        logo_path: '/uploads/categories/writing.svg',
+        logo_path: '/categories-logos/categories/writing.svg',
       },
     ])
     .execute();
