a9760e4c97ffcf2b701d747be0b72859e78caf4a
Mike Xiong <xiong.tchylon@gmail.com>
2023-12-11 21:57:36 +0100

    feat: create backend interaction

---
+++ packages/backend/api/src/interaction/interaction.ts
@@ -0,0 +1,76 @@
+import express from 'express';
+
+import { db } from '@app/backend-shared';
+
+const interactionRouter = express.Router();
+
+// Send a like
+interactionRouter.post('/:userId/interaction/like', async (req, res) => {
+  try {
+    const userId = Number.parseInt(req.params.userId);
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const { receiver_id } = req.body;
+
+    await db
+      .insertInto('user_action')
+      .values({
+        initiator_id: userId,
+        receiver_id,
+        liked_at: new Date(),
+        canceled_at: new Date(),
+      })
+      .execute();
+
+    res.json({ success: true });
+  } catch {
+    res.status(500).json({ success: false });
+  }
+});
+
+// Send a superlike
+interactionRouter.post('/:userId/interaction/superlike', async (req, res) => {
+  try {
+    const userId = Number.parseInt(req.params.userId);
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const { receiver_id } = req.body;
+
+    await db
+      .insertInto('user_action')
+      .values({
+        initiator_id: userId,
+        receiver_id,
+        superlike_at: new Date(),
+        canceled_at: new Date(),
+      })
+      .execute();
+
+    res.json({ success: true });
+  } catch {
+    res.status(500).json({ success: false });
+  }
+});
+
+// Next a user
+interactionRouter.post('/:userId/interaction/next', async (req, res) => {
+  try {
+    const userId = Number.parseInt(req.params.userId);
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const { receiver_id } = req.body;
+
+    await db
+      .insertInto('user_action')
+      .values({
+        initiator_id: userId,
+        receiver_id,
+        next_at: new Date(),
+        canceled_at: new Date(),
+      })
+      .execute();
+
+    res.json({ success: true });
+  } catch {
+    res.status(500).json({ success: false });
+  }
+});
+
+export default interactionRouter;
