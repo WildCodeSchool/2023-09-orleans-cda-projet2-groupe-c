f5b6695c8b6b50d2656bf1e89d77dda79a818373
Mike Xiong <xiong.tchylon@gmail.com>
2023-12-22 16:24:12 +0100

    feat: type interface

--- packages/backend/api/src/user/user-crud.ts
+++ packages/backend/api/src/user/user-crud.ts
@@ -1,41 +1,68 @@
 /* eslint-disable unicorn/no-null */
 import express from 'express';
+import { jsonArrayFrom } from 'kysely/helpers/mysql';
 
 import { db } from '@app/backend-shared';
-import type { Gender } from '@app/shared';
+import type { User } from '@app/shared';
 
 const userRouter = express.Router();
 
-type UserWithLanguages = {
-  id: number;
-  name: string | undefined;
-  birthdate: Date | undefined;
-  gender: Gender | undefined;
-  biography: string | undefined;
-  account_github: string | undefined;
-  city_id: number | undefined;
-  languages?: string[];
-  technologies?: string[];
-  hobbies?: string[];
-  pictures?: string[];
-};
-
-async function getUsers(userId: number) {
+async function getUsers(userId: number): Promise<User[]> {
   // Select all users with whom the initiator has not interacted
   //
   // MySQL query:
   //
-  // SELECT FROM u.name, u.birthdate, u.gender, u.biography, u.account_github, u.city_id
-  // FROM user AS u
-  // WHERE u.name IS NOT NULL AND u.birthdate IS NOT NULL AND u.gender IS NOT NULL
-  // AND u.city_id IS NOT NULL AND activate_at IS NOT NULL
-  // AND u.id IN NOT (
-  //      SELECT ua.receiver_id
-  //      FROM user_action AS ua
-  //      WHERE ua.initiator_id = userId
-  // );
+
   const users = await db
     .selectFrom('user as u')
+    .select((eb) => [
+      'u.id',
+      'u.name',
+      'u.birthdate',
+      'u.gender',
+      'u.biography',
+      'u.account_github',
+      jsonArrayFrom(
+        eb
+          .selectFrom('city as c')
+          .select(['c.id', 'c.name as city_name', 'c.coordinates'])
+          .whereRef('c.id', '=', 'u.city_id'),
+      ).as('city'),
+      jsonArrayFrom(
+        eb
+          .selectFrom('language_user as lu')
+          .innerJoin('language as l', 'lu.language_id', 'l.id')
+          .whereRef('lu.user_id', '=', 'u.id')
+          .select(['l.id', 'l.name', 'lu.order', 'l.logo_path'])
+          .orderBy('lu.order', 'asc')
+          .limit(6),
+      ).as('languages'),
+      jsonArrayFrom(
+        eb
+          .selectFrom('technology_user as tu')
+          .innerJoin('technology as t', 'tu.technology_id', 't.id')
+          .whereRef('tu.user_id', '=', 'u.id')
+          .select(['t.id', 't.name', 'tu.order', 't.logo_path'])
+          .orderBy('tu.order', 'asc')
+          .limit(6),
+      ).as('technologies'),
+      jsonArrayFrom(
+        eb
+          .selectFrom('hobby_user as hu')
+          .innerJoin('hobby as h', 'hu.hobby_id', 'h.id')
+          .innerJoin('hobby_category as hc', 'h.hobby_category_id', 'hc.id')
+          .whereRef('hu.user_id', '=', 'u.id')
+          .select(['hc.name as category', 'h.id', 'h.name', 'hu.order']),
+      ).as('hobbies'),
+      jsonArrayFrom(
+        eb
+          .selectFrom('picture as p')
+          .whereRef('p.user_id', '=', 'u.id')
+          .select(['p.id', 'p.order', 'p.picture_path'])
+          .orderBy('p.order', 'asc')
+          .limit(6),
+      ).as('pictures'),
+    ])
     .where((eb) =>
       eb.and([
         eb('u.name', 'is not', null),
@@ -51,65 +78,87 @@ async function getUsers(userId: number) {
         .select('ua.receiver_id')
         .where('ua.initiator_id', '=', userId),
     )
-    .select([
-      'u.id',
-      'u.name',
-      'u.birthdate',
-      'u.gender',
-      'u.biography',
-      'u.account_github',
-      'u.city_id',
-    ])
     .execute();
 
-  // Remove the current user from the list of users
-  const filteredUsers: UserWithLanguages[] = users.filter(
-    (user) => Number(user.id) !== userId,
-  );
-
-  // For each user
-  for (const filteredUser of filteredUsers) {
-    // Select all languages from a user
-    const languages = await db
-      .selectFrom('language_user as lu')
-      .innerJoin('language as l', 'lu.language_id', 'l.id')
-      .where('lu.user_id', '=', filteredUser.id)
-      .selectAll()
-      .execute();
-
-    // Select all technologies from a user
-    const technologies = await db
-      .selectFrom('technology_user as tu')
-      .innerJoin('technology as t', 'tu.technology_id', 't.id')
-      .where('tu.user_id', '=', filteredUser.id)
-      .selectAll()
-      .execute();
-
-    // Select all hobbies from a user
-    const hobbies = await db
-      .selectFrom('hobby_user as hu')
-      .innerJoin('hobby as h', 'hu.hobby_id', 'h.id')
-      .where('hu.user_id', '=', filteredUser.id)
-      .selectAll()
-      .execute();
-
-    // Select all pictures from a user
-    const pictures = await db
-      .selectFrom('picture as p')
-      .innerJoin('user as u', 'p.user_id', 'u.id')
-      .where('p.user_id', '=', filteredUser.id)
-      .selectAll()
-      .execute();
-
-    // Add languages, technologies, hobbies and pictures fields to the user
-    filteredUser.languages = languages.map((l) => l.name);
-    filteredUser.technologies = technologies.map((t) => t.name);
-    filteredUser.hobbies = hobbies.map((h) => h.name);
-    filteredUser.pictures = pictures.map((p) => p.picture_path);
-  }
-
-  return filteredUsers;
+  return users;
 }
+
+// const users = await db
+//   .selectFrom('user as u')
+//   .where((eb) =>
+//     eb.and([
+//       eb('u.name', 'is not', null),
+//       eb('u.birthdate', 'is not', null),
+//       eb('u.gender', 'is not', null),
+//       eb('u.city_id', 'is not', null),
+//       eb('u.activate_at', 'is not', null),
+//     ]),
+//   )
+//   .where('u.id', 'not in', (eb) =>
+//     eb
+//       .selectFrom('user_action as ua')
+//       .select('ua.receiver_id')
+//       .where('ua.initiator_id', '=', userId),
+//   )
+//   .select([
+//     'u.id',
+//     'u.name',
+//     'u.birthdate',
+//     'u.gender',
+//     'u.biography',
+//     'u.account_github',
+//     'u.city_id',
+//   ])
+//   .execute();
+
+// Remove the current user from the list of users
+//   const filteredUsers: UserWithLanguages[] = users.filter(
+//     (user) => Number(user.id) !== userId,
+//   );
+
+//   // For each user
+//   for (const filteredUser of filteredUsers) {
+//     // Select all languages from a user
+//     const languages = await db
+//       .selectFrom('language_user as lu')
+//       .innerJoin('language as l', 'lu.language_id', 'l.id')
+//       .where('lu.user_id', '=', filteredUser.id)
+//       .selectAll()
+//       .execute();
+
+//     // Select all technologies from a user
+//     const technologies = await db
+//       .selectFrom('technology_user as tu')
+//       .innerJoin('technology as t', 'tu.technology_id', 't.id')
+//       .where('tu.user_id', '=', filteredUser.id)
+//       .selectAll()
+//       .execute();
+
+//     // Select all hobbies from a user
+//     const hobbies = await db
+//       .selectFrom('hobby_user as hu')
+//       .innerJoin('hobby as h', 'hu.hobby_id', 'h.id')
+//       .where('hu.user_id', '=', filteredUser.id)
+//       .selectAll()
+//       .execute();
+
+//     // Select all pictures from a user
+//     const pictures = await db
+//       .selectFrom('picture as p')
+//       .innerJoin('user as u', 'p.user_id', 'u.id')
+//       .where('p.user_id', '=', filteredUser.id)
+//       .selectAll()
+//       .execute();
+
+//     // Add languages, technologies, hobbies and pictures fields to the user
+//     filteredUser.languages = languages.map((l) => l.name);
+//     filteredUser.technologies = technologies.map((t) => t.name);
+//     filteredUser.hobbies = hobbies.map((h) => h.name);
+//     filteredUser.pictures = pictures.map((p) => p.picture_path);
+//   }
+
+//   return filteredUsers;
+// }
 
 // Fetch a list of users without the user logged in and return the first user from the list
 userRouter.get('/:userId', async (req, res) => {
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,11 +1,63 @@
+import type {
+  CityTable,
+  HobbyTable,
+  LanguageTable,
+  PictureTable,
+  TechnologyTable,
+  UserTable,
+} from './database';
+
 export * from './database';
 export * from './auth';
 
-export type User = {
-  name: string;
-  email: string;
+// Extend "UserTable", remove unwanted properties and add a new id property
+export type UserBase = Omit<
+  UserTable,
+  | 'id'
+  | 'role'
+  | 'email'
+  | 'password'
+  | 'email_verified_at'
+  | 'activation_code'
+  | 'activate_at'
+  | 'city_id'
+> & { id: number };
+
+// Extend "CityTable", remove unwanted properties and add a new id and city_id property
+export type City = Omit<CityTable, 'id' | 'name'> & {
+  id: number;
+  city_name: string;
 };
 
-export interface SomeInterface {
-  someProperty: string;
+// Extend "LanguageTable", remove unwanted properties and add a new id and order property
+export type Language = Omit<LanguageTable, 'id'> & {
+  id: number;
+  order: number | undefined;
+};
+
+// Extend "TechnologyTable", remove unwanted properties and add a new id and order property
+export type Technology = Omit<TechnologyTable, 'id'> & {
+  id: number;
+  order: number | undefined;
+};
+
+// Extend "HobbyTable", remove unwanted properties and add a new id, order and category property
+export type Hobby = Omit<HobbyTable, 'id' | 'hobby_category_id'> & {
+  id: number;
+  order: number | undefined;
+  category: string;
+};
+
+// Extend "PictureTable", remove unwanted properties and add a new id property
+export type Picture = Omit<PictureTable, 'id' | 'user_id'> & {
+  id: number;
+};
+
+// Extend "UserBase" and
+export interface User extends UserBase {
+  city: City[] | undefined;
+  languages: Language[] | undefined;
+  technologies: Technology[] | undefined;
+  hobbies: Hobby[] | undefined;
+  pictures: Picture[] | undefined;
 }
