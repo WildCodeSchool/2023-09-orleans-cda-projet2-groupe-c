166a03fe33b7804989f7b93b7b1ce30cf08b740d
Mike Xiong <xiong.tchylon@gmail.com>
2024-01-19 15:36:49 +0100

    fix: add userId from the jwt and add a condition if there is no interaction

--- packages/backend/api/src/interaction/interaction-crud.ts
+++ packages/backend/api/src/interaction/interaction-crud.ts
@@ -9,6 +9,7 @@ import { getUserId } from '@/middlewares/auth-handlers';
 import { getSuperLikeCount } from '@/middlewares/interaction-handlers';
 
 interface Request extends ExpressRequest {
+  userId?: number;
   superLikesCount?: number;
 }
 
@@ -164,43 +165,54 @@ interactionRouter.post(
     }
   },
 
-  interactionRouter.delete('/:userId/interactions/back', async (req, res) => {
-    try {
-      // Get the userId from the request
-      const userId = 1;
+  interactionRouter.delete(
+    '/:userId/interactions/back',
+    getUserId,
+    async (req: Request, res) => {
+      try {
+        // Get the userId from the request
+        const userId = req.userId as number;
 
-      // Get all interactions from the user logged in
-      const result = await db
-        .selectFrom('user_action as ua')
-        .innerJoin('user as initiator', 'initiator.id', 'ua.initiator_id')
-        .innerJoin('user as receiver', 'receiver.id', 'ua.receiver_id')
-        .where('ua.initiator_id', '=', userId)
-        .where('ua.canceled_at', 'is not', null)
-        .select([
-          'ua.id',
-          'receiver.name',
-          'ua.liked_at',
-          'ua.next_at',
-          'ua.superlike_at',
-          'ua.canceled_at',
-        ])
-        .execute();
+        // Get all interactions from the user logged in
+        const result = await db
+          .selectFrom('user_action as ua')
+          .innerJoin('user as initiator', 'initiator.id', 'ua.initiator_id')
+          .innerJoin('user as receiver', 'receiver.id', 'ua.receiver_id')
+          .where('ua.canceled_at', 'is not', null)
+          .where('ua.initiator_id', '=', userId)
+          .select([
+            'ua.id',
+            'receiver.name',
+            'ua.liked_at',
+            'ua.next_at',
+            'ua.superlike_at',
+            'ua.canceled_at',
+          ])
+          .execute();
 
-      // Get the last action from the user logged in
-      const index = 1;
-      const lastAction = result[result.length - index];
+        // If there is no interaction to delete, returns an error
+        if (result.length === 0) {
+          return res
+            .status(400)
+            .json({ success: false, error: 'No interaction to delete !' });
+        }
 
-      // Delete the last action from the user logged in
-      await db
-        .deleteFrom('user_action as ua')
-        .where('id', '=', lastAction.id)
-        .execute();
+        // Get the last action from the user logged in
+        const index = 1;
+        const lastAction = result[result.length - index];
 
-      res.json({ success: true });
-    } catch {
-      throw new Error('An error occurred during user interaction.');
-    }
-  }),
+        // Delete the last action from the user logged in
+        await db
+          .deleteFrom('user_action as ua')
+          .where('id', '=', lastAction.id)
+          .execute();
+
+        res.json({ success: true });
+      } catch {
+        throw new Error('An error occurred during user interaction.');
+      }
+    },
+  ),
 );
 
 export default interactionRouter;
