c3ff1e93a57c11efe9bcaff4a1e987780247412d
greegouze <gregory.silvestri45@gmail.com>
2024-01-28 13:46:41 +0100

    fetch api on front

--- packages/backend/api/src/user/message.ts
+++ packages/backend/api/src/user/message.ts
@@ -16,118 +16,138 @@ messageRouter.get(
     //this block will change with the middleware, from an other pr
     try {
       const conversationId = Number.parseInt(req.params.conversationId);
-
+      
       const { userId } = req.params;
       const userIniator = req.userId as number;
-
+      
       if (Number(userId) !== userIniator) {
         return res.status(403).send('Unauthorized');
       }
-
+      
       const messages = await db
-        .selectFrom('message as m')
-        .innerJoin('user as u', 'm.sender_id', 'u.id')
-        .select(['m.id', 'u.name as sender_name', 'm.content', 'm.sent_at'])
-        .where('m.conversation_id', '=', conversationId)
-        .where('u.id', '=', Number(userId))
-        .orderBy('m.sent_at', 'desc')
-        .execute();
-
-      return res.json(messages);
-    } catch (error) {
-      throw new Error(`Fail to get messages : ${String(error)}`);
-    }
-  },
-);
-
-messageRouter.get(
-  '/:userId/conversations/:conversationId/last-message',
-  getUserId,
-  async (req: Request, res) => {
-    //1er étape récuper l'id des conversations
-    const { conversationId } = req.params;
-    const userIniator = req.userId as number;
-    const { userId } = req.params;
-
-    if (Number(userId) !== userIniator) {
-      return res.status(403).send('Unauthorized');
-    }
-
-    //2eme etape récuperer le dernier message d'une conversation
-
-    const lastMessage = await db
       .selectFrom('message as m')
       .innerJoin('user as u', 'm.sender_id', 'u.id')
-      .innerJoin('user_action as ua', 'm.conversation_id', 'ua.id')
+      .select(['m.id', 'u.name as sender_name', 'm.content', 'm.sent_at'])
       .select((eb) => [
-        'm.conversation_id',
         jsonObjectFrom(
           eb
-            .selectFrom('user as u')
-            .innerJoin('picture as p', 'p.user_id', 'u.id')
-            .select(['u.id', 'u.name', 'p.picture_path'])
-            .whereRef('u.id', '=', 'm.sender_id')
-            .orderBy('p.id asc')
-            .limit(1),
-        ).as('sender'),
-        jsonObjectFrom(
-          eb
+          .selectFrom('user as u')
+          .innerJoin('picture as p', 'p.user_id', 'u.id')
+          .select(['p.picture_path'])
+          .whereRef('u.id', '=', 'm.sender_id')
+          
+          ).as('sender'),
+          jsonObjectFrom(
+            eb
             .selectFrom('user_action as ua')
             .innerJoin('user as u', 'ua.receiver_id', 'u.id')
             .innerJoin('picture as p', 'p.user_id', 'u.id')
-            .select(['u.id', 'u.name', 'p.picture_path'])
+            .select(['p.picture_path', 'u.name'])
             .whereRef('m.conversation_id', '=', 'ua.id')
-            .orderBy('p.id asc')
-            .limit(1),
-        ).as('receiver'),
-        jsonObjectFrom(
-          eb
-            .selectFrom('message as m')
-            .innerJoin('user as u', 'm.sender_id', 'u.id')
-            .whereRef('m.conversation_id', '=', 'ua.id')
-            .select(['m.id', 'u.name as sender_name', 'm.content', 'm.sent_at'])
-            .orderBy('m.sent_at', 'desc')
-            .limit(1),
-        ).as('messages'),
-      ])
-      .where('sender_id', '=', Number(userId))
-      .where('m.conversation_id', '=', Number(conversationId))
-      .groupBy('m.conversation_id')
-      .execute();
-
-    return res.json(lastMessage);
-  },
-);
-
-messageRouter.post(
-  '/:userId/conversations/:conversationId/message',
-  getUserId,
-  verifyConversation,
-  async (req: Request, res) => {
-    try {
-      const { content } = req.body;
-      const { conversationId } = req.params;
-      const senderId = req.userId as number;
-
-      await db
-        .insertInto('message')
-        .values({
-          content,
-          sent_at: new Date(), // current date and time
-          conversation_id: Number(conversationId),
-          sender_id: senderId,
-        })
-        .execute();
-
-      return res.json({ succes: true, message: 'Message sent successfully' });
-    } catch {
-      return res.status(500).json({
-        success: false,
-        error: 'An error occurred while sending the message',
-      });
-    }
-  },
-);
-
-//todo get route for matching card
-export default messageRouter;
+            ).as('receiver'),
+          ])
+          
+          .where('m.conversation_id', '=', conversationId)
+          .where('u.id', '=', Number(userId))
+          .orderBy('m.sent_at', 'desc')
+          .execute();
+          
+          return res.json(messages);
+        } catch (error) {
+          throw new Error(`Fail to get messages : ${String(error)}`);
+        }
+      },
+      );
+      
+      messageRouter.get(
+        '/:userId/conversations/:conversationId/last-message',
+        getUserId,
+        async (req: Request, res) => {
+          //1er étape récuper l'id des conversations
+          const { conversationId } = req.params;
+          const userIniator = req.userId as number;
+          const { userId } = req.params;
+          
+          if (Number(userId) !== userIniator) {
+            return res.status(403).send('Unauthorized');
+          }
+          
+          //2eme etape récuperer le dernier message d'une conversation
+          
+          const lastMessage = await db
+          .selectFrom('message as m')
+          .innerJoin('user as u', 'm.sender_id', 'u.id')
+          .innerJoin('user_action as ua', 'm.conversation_id', 'ua.id')
+          .select((eb) => [
+            'm.conversation_id',
+            jsonObjectFrom(
+              eb
+              .selectFrom('user as u')
+              .innerJoin('picture as p', 'p.user_id', 'u.id')
+              .select(['u.id', 'u.name', 'p.picture_path'])
+              .whereRef('u.id', '=', 'm.sender_id')
+              .orderBy('p.id asc')
+              .limit(1),
+              ).as('sender'),
+              jsonObjectFrom(
+                eb
+                .selectFrom('user_action as ua')
+                .innerJoin('user as u', 'ua.receiver_id', 'u.id')
+                .innerJoin('picture as p', 'p.user_id', 'u.id')
+                .select(['u.id', 'u.name', 'p.picture_path'])
+                .whereRef('m.conversation_id', '=', 'ua.id')
+                .orderBy('p.id asc')
+                .limit(1),
+                ).as('receiver'),
+                jsonObjectFrom(
+                  eb
+                  .selectFrom('message as m')
+                  .innerJoin('user as u', 'm.sender_id', 'u.id')
+                  .whereRef('m.conversation_id', '=', 'ua.id')
+                  .select(['m.id', 'u.name as sender_name', 'm.content', 'm.sent_at'])
+                  .orderBy('m.sent_at', 'desc')
+                  .limit(1),
+                  ).as('messages'),
+                ])
+                .where('sender_id', '=', Number(userId))
+                .where('m.conversation_id', '=', Number(conversationId))
+                .groupBy('m.conversation_id')
+                .execute();
+                
+                return res.json(lastMessage);
+              },
+              );
+              
+              messageRouter.post(
+                '/:userId/conversations/:conversationId/message',
+                getUserId,
+                verifyConversation,
+                async (req: Request, res) => {
+                  try {
+                    const { content } = req.body;
+                    const { conversationId } = req.params;
+                    const senderId = req.userId as number;
+                    
+                    await db
+                    .insertInto('message')
+                    .values({
+                      content,
+                      sent_at: new Date(),
+                      conversation_id: Number(conversationId),
+                      sender_id: senderId,
+                    })
+                    .execute();
+                    
+                    return res.json({ succes: true, message: 'Message sent successfully' });
+                  } catch {
+                    return res.status(500).json({
+                      success: false,
+                      error: 'An error occurred while sending the message',
+                    });
+                  }
+                },
+                );
+                
+                //todo get route for matching card
+                export default messageRouter;
+                
--- packages/frontend/web/src/components/message/Conversation.tsx
+++ packages/frontend/web/src/components/message/Conversation.tsx
@@ -1,14 +1,58 @@
+import { useEffect, useState } from 'react';
+
+import { useAuth } from '@/contexts/AuthContext';
+
+import CrossIcon from '../icons/CrossIcon';
+import SendIcon from '../icons/SendIcon';
+
 export default function Conversation() {
+  const { userId } = useAuth();
+  const [users, setUsers] = useState();
+  console.log(users);
+
+  console.log(users?.receiver.name);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    (async () => {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/users/${userId}/messages/1`,
+        {
+          signal: controller.signal,
+          credentials: 'include',
+        },
+      );
+      const data = await response.json();
+      setUsers(data[0]);
+    })();
+
+    return () => {
+      controller.abort();
+    };
+  }, [userId]);
+
   return (
     <div className='absolute right-0 z-50 ml-auto flex h-[calc(100vh-56px)] w-[75%] bg-red-700'>
-      {/*  sidebar */}
-      {/* <div className='h-full w-[25%] bg-blue-500'></div> */}
-      {/*  sidebar */}
-      <div className='h-full flex-grow bg-pink-700'>
-        <div className='h-14 w-full bg-white' />
-        <div className='flex h-[calc(100%-3.5rem)] w-full flex-col justify-end gap-5 bg-green-900 p-3'>
-          <div className='h-full overflow-y-auto bg-red-600'>
-            <div className={`flex flex-col-reverse gap-6 overflow-y-auto`}>
+      <div className='h-full w-full flex-grow bg-blue-600'>
+        <div className='flex h-14 w-full items-center justify-between bg-[#3f436a] p-3'>
+          <div className='flex items-center gap-3 text-white'>
+            <img
+              src={users?.receiver.picture_path}
+              alt=''
+              className='h-9 w-9 rounded-full'
+            />
+            <p>{users?.receiver?.name}</p>
+          </div>
+          <div>
+            <CrossIcon className='h-5 fill-white' />
+          </div>
+        </div>
+        <div className='flex h-[calc(100%-3.5rem)] w-full flex-col gap-5 bg-green-900 p-3'>
+          <div className='h-full w-full overflow-y-auto bg-red-600'>
+            <div
+              className={`flex flex-col-reverse gap-6 overflow-y-auto bg-slate-500`}
+            >
               <div className='h-12 w-1/2  bg-indigo-900' />
               <div className='h-12 w-1/2 bg-white' />
               <div className='h-12 w-1/2 bg-indigo-900' />
@@ -30,18 +74,41 @@ export default function Conversation() {
               <div className='h-12 w-1/2 bg-indigo-900' />
               <div className='h-12 w-1/2 bg-white' />
               <div className='h-12 w-1/2 bg-indigo-900' />
-              <div className='h-12 w-1/2 bg-white' />
+
+              {/* bullet  */}
+              <div className='m-2 flex flex-row-reverse items-center gap-3'>
+                <p>yolo</p>
+
+                <div className='min-w-1/2 min-h-12 max-w-[75%] break-words rounded-2xl bg-white p-3'>
+                  <p className=''>
+                    yololllllllllllllllllllllllllllllllllll
+                    llllllllllllllllllllllllllllllllllllllllllllllllllllll
+                    lllllllllllllllllllllllllllllliiiiiiiiiiiiiiiiiiiiiiiiiiiii
+                    iiiiiiiiiii iiiiiiiiiiiiiMMMMMMMMMMMMMMMM MMMMMMMMMMMMM
+                    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM MMMMMMMMMM
+                    MMMMMMMMMMMMMMMMMMMMM
+                  </p>
+                  <div className='text-placeholder pt-1'>
+                    <p>coucou</p>
+                  </div>
+                </div>
+                {/* bullet  */}
+              </div>
             </div>
           </div>
+
           {/*  text */}
 
           <form
             action=''
-            className='relative flex h-12 w-full items-center bg-black'
+            className='relative flex h-10 w-full items-center bg-black'
           >
-            <textarea className='flex h-full w-full resize-none items-center rounded-l-full bg-white p-3' />
-            <div className='flex h-full w-20 items-center justify-center rounded-r-full bg-slate-600'>
-              <p>{'yoo'}</p>
+            <textarea
+              className='flex h-full w-full resize-none items-center rounded-l-full border-none bg-white pl-4 pt-2 outline-none'
+              placeholder='Aa'
+            />
+            <div className='bg-light flex h-full w-12 items-center justify-center rounded-r-full'>
+              <SendIcon className='fill-light h-full' />
             </div>
           </form>
         </div>
