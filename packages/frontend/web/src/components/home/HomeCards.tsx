e0dad8126213748ad787dfcac8c4509db644f4e8
Mike Xiong <xiong.tchylon@gmail.com>
2024-01-30 17:11:12 +0100

    feat: create backend upload

--- packages/frontend/web/src/components/home/HomeCards.tsx
+++ packages/frontend/web/src/components/home/HomeCards.tsx
@@ -14,7 +14,6 @@ export default function HomeCards() {
 
   return (
     <div className='font-base mx-auto flex h-[calc(100vh-56px)] w-full max-w-[500px] flex-col justify-between gap-5 overflow-y-auto px-5 py-10 text-white'>
-      <FormPicture />
       <Card user={selectedUser} />
       <Interactions />
     </div>
