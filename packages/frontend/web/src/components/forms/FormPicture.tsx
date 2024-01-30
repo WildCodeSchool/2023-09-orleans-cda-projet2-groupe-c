734cc5a867c69c694e2b3877d39bfc5518fc832b
Mike Xiong <xiong.tchylon@gmail.com>
2024-01-30 22:53:15 +0100

    feat: add validation file size and type

--- packages/frontend/web/src/components/forms/FormPicture.tsx
+++ packages/frontend/web/src/components/forms/FormPicture.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { type SubmitHandler, useForm } from 'react-hook-form';
 
 const API_URL = import.meta.env.VITE_API_URL;
@@ -7,34 +8,42 @@ interface FormPictureBody {
   picture: FileList;
 }
 
-const onSubmit: SubmitHandler<FormPictureBody> = (data) => {
-  //
-  const formData = new FormData();
-  formData.append('picture', data.picture[0]);
-
-  const uploadPicture = async () => {
-    await fetch(`${API_URL}/registration/upload`, {
-      method: 'POST',
-      credentials: 'include',
-      body: formData,
-    });
-  };
-
-  uploadPicture().catch(() => {
-    throw new Error('Fail to upload picture.');
-  });
-};
-
 export default function FormPicture() {
   const { register, handleSubmit } = useForm<FormPictureBody>();
+  const [error, setError] = useState<string | undefined>();
+
+  const onSubmit: SubmitHandler<FormPictureBody> = (data) => {
+    //
+    const formData = new FormData();
+    formData.append('picture', data.picture[0]);
+
+    const uploadPicture = async () => {
+      const res = await fetch(`${API_URL}/registration/upload`, {
+        method: 'POST',
+        credentials: 'include',
+        body: formData,
+      });
+
+      const resData = await res.json();
+
+      if (!Boolean(resData.success)) {
+        setError(resData.message);
+      }
+    };
+
+    uploadPicture().catch(() => {
+      setError('Fail to upload picture.');
+    });
+  };
 
   return (
     <div>
       <img
-        src={`${BACKEND_URL}/uploads/a014d44c-56a1-4ef1-9f91-1a0476f284ac-20231209_131738.jpg`}
+        src={`${BACKEND_URL}/uploads/5feaa783-1b2b-44d7-a886-16b9fcad55d6-20231209_131738 (1).jpg`}
         alt='photo test'
         className='h-40 w-40'
       />
+      <p className='text-red-500'>{error}</p>
       <form onSubmit={handleSubmit(onSubmit)} encType='multipart/form-data'>
         <input
           {...register('picture')}
--- packages/frontend/web/src/components/home/HomeCards.tsx
+++ packages/frontend/web/src/components/home/HomeCards.tsx
@@ -1,6 +1,7 @@
 import { useInteraction } from '@/contexts/InteractionContext';
 
 import Loading from '../Loading';
+import FormPicture from '../forms/FormPicture';
 import Card from './Card';
 import Interactions from './Interactions';
 
@@ -13,6 +14,7 @@ export default function HomeCards() {
 
   return (
     <div className='font-base mx-auto flex h-[calc(100vh-56px)] w-full max-w-[500px] flex-col justify-between gap-5 overflow-y-auto px-5 py-10 text-white'>
+      <FormPicture />
       <Card user={selectedUser} />
       <Interactions />
     </div>
