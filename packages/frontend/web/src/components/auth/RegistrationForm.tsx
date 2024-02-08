9bd05ea18e4641adb20ed7d3d26bcbf80df6010e
Mike Xiong <xiong.tchylon@gmail.com>
2024-02-08 10:10:37 +0100

    fix: update registation and use FormContainer

--- packages/frontend/web/src/components/auth/RegistrationForm.tsx
+++ packages/frontend/web/src/components/auth/RegistrationForm.tsx
@@ -9,6 +9,7 @@ import { registrationSchema } from '@app/shared';
 import { useAuth } from '@/contexts/AuthContext';
 
 import Button from '../Button';
+import FormContainer from '../forms/FormContainer';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -55,66 +56,61 @@ export default function RegistrationForm() {
   };
 
   return (
-    <div className='p-8'>
-      <h1 className='font-title text-primary mb-5 mt-24 text-xl'>
-        {'Create a new account'}
-      </h1>
-      <div className='bg-light-light flex h-[18rem] flex-col items-center gap-2 rounded-lg px-2 shadow-md'>
-        <div className='text-start'>
-          <p className='text-secondary my-2 mb-4 align-top text-xs'>
-            {'Please enter your credentials to continue.'}
-          </p>
-        </div>
-        <div className='flex flex-col items-center'>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className='flex flex-col justify-center gap-3'
-          >
-            <label htmlFor='email' className='text-secondary -mb-2 text-sm'>
-              {'Email'}
-              <span className='text-primary'>{'*'}</span>
-            </label>
-            <input
-              type='email'
-              id='email'
-              className='border-primary bg-light-light text-secondary focus:outline-secondary w-100 rounded-lg border p-2 text-center transition-all focus:outline'
-              {...register('email')}
-            />
-            {errors.email && errors.email.message !== undefined ? (
-              <p className='text-primary flex text-xs'>
-                {errors.email.message}
-              </p>
-            ) : undefined}
-            <label
-              htmlFor='password'
-              className='text-secondary -mb-2 mt-2 text-sm'
-            >
-              {'Password'}
-              <span className='text-primary'>{'*'}</span>
-            </label>
-            <input
-              type='password'
-              id='password'
-              className='border-primary bg-light-light text-secondary focus:outline-secondary w-100 rounded-lg border p-2 text-center transition-all focus:outline'
-              {...register('password')}
-            />
+    <div className='mx-auto flex h-full w-full max-w-[500px] flex-col justify-between'>
+      <FormContainer title='Create a new account'>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className='flex h-full flex-col items-center justify-between'
+        >
+          <div className='flex h-full w-full flex-col justify-between gap-6'>
+            <div className='flex flex-col gap-1'>
+              <label htmlFor='email' className='text-secondary text-sm'>
+                {'Email'}
+                <span className='text-primary'>{'*'}</span>
+              </label>
+              <input
+                type='email'
+                id='email'
+                className='border-primary bg-light-light text-secondary focus:outline-secondary rounded-lg border p-2 text-center transition-all focus:outline'
+                {...register('email')}
+              />
 
-            {errors.password && errors.password.message !== undefined ? (
-              <p className='text-primary flex text-xs'>
-                {errors.password.message}
-              </p>
-            ) : undefined}
-
-            {Boolean(errorRegistration) && <p>{errorRegistration}</p>}
-
-            <div className='mt-[20rem] flex flex-col'>
-              <Button type='submit' isOutline={false}>
-                {'Validate'}
-              </Button>
+              {errors.email && errors.email.message !== undefined ? (
+                <p className='text-primary flex text-xs'>
+                  {errors.email.message}
+                </p>
+              ) : undefined}
             </div>
-          </form>
-        </div>
-      </div>
+
+            <div className='flex flex-col gap-1'>
+              <label htmlFor='password' className='text-secondary mt-2 text-sm'>
+                {'Password'}
+                <span className='text-primary'>{'*'}</span>
+              </label>
+              <input
+                type='password'
+                id='password'
+                className='border-primary bg-light-light text-secondary focus:outline-secondary w-100 rounded-lg border p-2 text-center transition-all focus:outline'
+                {...register('password')}
+              />
+            </div>
+          </div>
+
+          {errors.password && errors.password.message !== undefined ? (
+            <p className='text-primary flex text-xs'>
+              {errors.password.message}
+            </p>
+          ) : undefined}
+
+          {Boolean(errorRegistration) && <p>{errorRegistration}</p>}
+
+          <div className='mt-20 flex w-full flex-col pb-12'>
+            <Button type='submit' isOutline={false}>
+              {'Validate'}
+            </Button>
+          </div>
+        </form>
+      </FormContainer>
     </div>
   );
 }
