37602c9c2b881821bc0e10a529a470e7145f3fae
greegouze <gregory.silvestri45@gmail.com>
2024-02-28 16:09:55 +0100

    remove comments

--- packages/frontend/web/src/components/message/Conversation.tsx
+++ packages/frontend/web/src/components/message/Conversation.tsx
@@ -6,7 +6,6 @@ import { useNavigate } from 'react-router-dom';
 import { type MessageValidation, messageSchema } from '@app/shared';
 
 import { useConversation } from '@/contexts/ConversationContext';
-import useAllConversations from '@/hooks/use-all-conversations';
 
 import CrossIcon from '../icons/CrossIcon';
 import SendIcon from '../icons/SendIcon';
